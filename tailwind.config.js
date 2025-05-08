649590b4ecbdbc7af196cff2f1a776cd88232592
Amir Asyraaf <amir.asyraaf@cibc-technology.com>
2025-05-08 13:02:09 +0800

    color setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,13 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#ED4139',
+        secondary: '#292986',
+        background: '#FFFFFF',
+      },
+    },
   },
   plugins: [],
 } 
